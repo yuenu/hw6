2ee41aed3293a7f5d1fa17bfb2265aeb0a50d8a7
yuenu <miraiku82@gmail.com>
2022-07-21 11:50:32 +0800

    chore: remove store

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
 
@@ -9,7 +7,5 @@ const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
     <App />
-  </Provider>
 );
